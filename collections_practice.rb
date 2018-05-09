7adc6ff1489d04ae3f9bfacf6e22907e6986ba2c
Scott Silverman <scottms724@gmail.com>
2018-05-09 15:41:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,12 +47,13 @@ def sum_array(integer_array)
 end
 
 def add_s(array)
+  new_array = []
   array.collect do |element| 
     if not array[1]
       element << "s"
     end 
   end 
-  array 
+  
 end
 
 
